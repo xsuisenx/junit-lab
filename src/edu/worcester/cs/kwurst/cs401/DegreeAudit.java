320bdf05b7a09cdd13f6cf9b9fd6c17fb8dd5737
Karl R. Wurst <karl@w-sts.com>
2014-03-27 15:57:15 -0400

    Make Eclipse project and initial commit.

---
+++ src/edu/worcester/cs/kwurst/cs401/DegreeAudit.java
@@ -0,0 +1,69 @@
+package edu.worcester.cs.kwurst.cs401;
+
+/* 
+ * Copyright (C) 2014 Karl R. Wurst
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+/**
+ * Creates student objects and calls methods on them.
+ * 
+ * @author Karl R. Wurst 
+ * @version JUnit Lab
+ */
+public class DegreeAudit
+{
+    /**
+     * Creates Student objects and calls methods on them to test the methods.
+     * 
+     * @param args commandline arguments (not used)
+     */
+    public static void main(String[] args) {
+        // Check static constant representing the number of credits required to graduate.
+        System.out.println("credits to graduate = " + Student.REQUIRED_CR);
+        // Initial check of number of student created.
+        System.out.println("Number of students " + Student.getStudentCount());
+        
+        // Create a student.
+        Student s1 = new Student("Jane", "Smith");
+        // Print the student's ID.
+        System.out.println("ID " + s1.getId());
+        // Check count of created students.
+        System.out.println("Number of students " + Student.getStudentCount());
+
+        // Set some characteristics of the student.
+        s1.setGpa(2.5);
+        s1.setCurrentEarnedCr(110);
+        // Check number of credits remaining.
+        System.out.println(s1.getCurrentRemainingCr());
+        // Change credits.
+        s1.setCurrentEarnedCr(120);
+        // Check number of credits remaining.
+        System.out.println(s1.getCurrentRemainingCr());
+        
+        // Set LASC and major requirement to be complete.
+        s1.setMajorComplete(true);
+        s1.setLascComplete(true);
+        // Is the student ready to graduate?
+        System.out.println("Ready to graduate: " + s1.readyToGraduate());
+        
+        // Create another student.
+        Student s2 = new Student("John", "Smith");
+        // Check that the ID number has gone up.
+        System.out.println("ID " + s2.getId());
+        //Check that the number of students has gone up.
+        System.out.println("Number of students " + Student.getStudentCount());
+    }
+}
---
+++ src/edu/worcester/cs/kwurst/cs401/Student.java
@@ -0,0 +1,243 @@
+package edu.worcester.cs.kwurst.cs401;
+/* 
+ * Copyright (C) 2014 Karl R. Wurst
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+/**
+ * Represents a student record for the purposes of a degree audit.
+ * 
+ * @author Karl R. Wurst
+ * @version JUnit Lab
+ */
+public class Student
+{
+    // Information about the individual student
+    private double gpa;
+    private String firstName, lastName, id;
+    private int currentEarnedCr; // Current earned credits (already completed)
+    private int anticipatedAdditionalCr; // Anticipated additional credits (currently taking)
+    private boolean lascComplete; // Has the student completed LASC requirements
+    private boolean majorComplete; //Has the student completed requirements for the major
+
+    // Minimum number of credits required to graduate
+    public static final int REQUIRED_CR = 120;
+    
+    // Keeps track of the id number to assign to the next student to be created
+    private static int nextId = 1;
+
+    /**
+     * Creates a new student given a first and last name. An id number is assigned sequentially.
+     * 
+     * @param firstName the student's first name
+     * @param lastName the student's last name
+     */
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = String.format("%07d",nextId); // 7 digits wide, zero padded
+        nextId++;
+    }
+
+    /**
+     * Returns the number of students which have been created.
+     * 
+     * @return the number of students which have been created
+     */
+    public static int getStudentCount() {
+        return nextId-1;
+    }
+    
+    /**
+     * Set the GPA of the student.
+     * 
+     * @param gpa the student's GPA
+     */
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    /**
+     * Returns the student's GPA.
+     * 
+     * @return the student's GPA
+     */
+    public double getGpa() {
+        return gpa;
+    }
+
+    /**
+     * Sets the student's first name.
+     * 
+     * @param firstName the student's first name
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * Returns the student's first name.
+     * 
+     * @returns the student's first name
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Sets the student's last name.
+     *
+     * @param lastName the student's last name
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * Returns the student's last name.
+     * 
+     * @returns the student's last name
+     */    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    /**
+     * Returns the student's ID number.
+     * 
+     * @returns the student's ID number
+     */    
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Sets the student's current earned credits.
+     * (This should really be done by looking at a 
+     * list of completed courses, not by setting it directly.)
+     * 
+     * @param currentEarnedCr the number of current earned credits
+     */
+    public void setCurrentEarnedCr(int currentEarnedCr) {
+        this.currentEarnedCr = currentEarnedCr;
+    }
+
+    /**
+     * Returns the student's current earned credits.
+     * 
+     * @return the student's current earned credits
+     */
+    public int getCurrentEarnedCr() {
+        // This should really be done by looking at a 
+        // list of completed courses, not by returning a variable.
+        return currentEarnedCr;
+    }
+    
+    /**
+     * Sets the student's anticipated additional credits (the 
+     * courses they are currently taking).
+     * (This should really be done by looking at a 
+     * list of registered courses, not by setting it directly.)
+     * 
+     * @param anticipatedAdditionalCr the number of anticipated additional credits
+     */
+    public void setAnticipatedAdditionalCr(int anticipatedAdditionalCr) {
+        this.anticipatedAdditionalCr = anticipatedAdditionalCr;
+    }
+
+    /**
+     * Returns the student's anticipated additional credits.
+     * 
+     * @return the student's anticipated additional credits
+     */
+    public int getAnticipatedAdditionalCr() {
+        // This should really be done by looking at a 
+        // list of registered courses, not by returning a variable.
+        return anticipatedAdditionalCr;
+    }
+
+    /**
+     * Sets whether the student has completed the LASC requirements.
+     * (This should really be done by looking at a 
+     * list of completed courses, not by setting it directly.)
+     * 
+     * @param lascComplete whether LASC requirements are complete
+     */
+    public void setLascComplete(boolean lascComplete) {
+        this.lascComplete = lascComplete;
+    }
+
+    /**
+     * Returns whether the student has completed the LASC requirements.
+     * 
+     * @return whether the student has completed the LASC requirements
+     */
+    public boolean getLascComplete() {
+        // This should really be done by looking at a 
+        // list of complete courses, not by returning a variable.
+        return lascComplete;
+    }
+
+    /**
+     * Sets whether the student has completed the major requirements.
+     * (This should really be done by looking at a 
+     * list of completed courses, not by setting it directly.)
+     * 
+     * @param majorComplete whether major requirements are complete
+     */
+    public void setMajorComplete(boolean majorComplete) {
+        this.majorComplete = majorComplete;
+    }
+
+    /**
+     * Returns whether the student has completed the major requirements.
+     * 
+     * @return whether the student has completed the major requirements
+     */
+    public boolean getMajorComplete() {
+        // This should really be done by looking at a 
+        // list of complete courses, not by returning a variable.
+        return majorComplete;
+    }
+    
+    /**
+     * Returns the student's remaining credits to graduate
+     * (not including the courses they are currently taking).
+     * 
+     * @return the student's remaining credits to graduate
+     */
+    public int getCurrentRemainingCr() {
+        return REQUIRED_CR - currentEarnedCr;
+    }
+
+    /**
+     * Returns the student's anticipated remaining credits to graduate
+     * (including the courses they are currently taking).
+     * 
+     * @return the student's anticipated remaining credits to graduate
+     */
+    public int getAnticipatedRemainingCr() {
+        return getCurrentRemainingCr() - anticipatedAdditionalCr;
+    }
+
+    /**
+     * Returns whether the student is ready to graduate.
+     * 
+     * @return whether the student is ready to graduate
+     */
+    public boolean readyToGraduate() {
+        return getCurrentRemainingCr() == 0 && gpa >= 2.0 && lascComplete && majorComplete;
+    }
+}
